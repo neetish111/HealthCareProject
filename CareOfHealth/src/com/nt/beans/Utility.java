f57cb55f5a5f7d84550fd06d8f4a2c96ea71be2d
neetish <neetish@DESKTOP-NKPNROV>
2020-04-10 21:40:37 +0530

    initial commit and class creation

---
+++ CareOfHealth/src/com/nt/beans/Utility.java
@@ -0,0 +1,12 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+public class Utility {
+	Date d=new Date();
+	public int getDate()
+	{
+		return d.getDay();
+	}
+
+}
---
+++ CareOfHealth/src/com/nt/test/Find.java
@@ -0,0 +1,11 @@
+package com.nt.test;
+
+import com.nt.beans.Utility;
+
+public class Find {
+	public static void main(String[] args) {
+		Utility ut=new Utility();
+		System.out.println("date is"+ut.getDate());
+	}
+
+}
