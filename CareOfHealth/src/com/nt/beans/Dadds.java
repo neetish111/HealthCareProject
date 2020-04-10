f54cb2fbd4f1d2005cd32d4f3408d9961aa7e45a
neetish <neetish@DESKTOP-NKPNROV>
2020-04-10 22:07:32 +0530

    Daddu related class completed

---
+++ CareOfHealth/src/com/nt/beans/Dadds.java
@@ -0,0 +1,8 @@
+package com.nt.beans;
+
+public class Dadds {
+public void getSomethingForSure()
+{
+	System.out.println("get up in morning");
+}
+}
---
+++ CareOfHealth/src/com/nt/beans/Daddu.java
@@ -0,0 +1,8 @@
+package com.nt.beans;
+
+public class Daddu {
+public void getSomething()
+{
+	System.out.println("Hii Daddu");
+}
+}
